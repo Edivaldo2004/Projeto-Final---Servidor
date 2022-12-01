d29f03f4184910d973ea1de53cdaf11ec02bb687
Edivaldo <edivaldosantosnior2004@gmail.com>
2022-12-01 14:16:21 -0400

    Mexendo no .env, para funcionar o cyclic

--- config/config.js
+++ config/config.js
@@ -2,21 +2,21 @@ require('dotenv/config');
 
 module.exports = {
   development: {
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    host: process.env.HOST,
-    port: process.env.PORT,
-    dialect: process.env.DIALECT
+    username: process.env.DBUSERNAME,
+    password: process.env.DBPASSWORD,
+    database: process.env.DBDATABASE,
+    host: process.env.DBHOST,
+    port: process.env.DBPORT,
+    dialect: process.env.DBDIALECT
   },
 
   test: {
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    host: process.env.HOST,
-    port: process.env.PORT,
-    dialect: process.env.DIALECT
+    username: process.env.DBUSERNAME,
+    password: process.env.DBPASSWORD,
+    database: process.env.DBDATABASE,
+    host: process.env.DBHOST,
+    port: process.env.DBPORT,
+    dialect: process.env.DBDIALECT
   },
   
   production: {
